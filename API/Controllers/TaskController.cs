238e99e3733c7f1e0f4c2ac51ff839ab14e7127e
israel.vieira <israel.vieira@estudante.ufla.br>
2022-08-06 15:43:46 -0300

    Initial Project

---
+++ API/Controllers/TaskController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Workspace.API.Data;
+using Workspace.API.Models;
+
+namespace Workspace.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TaskController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public TaskController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Task> Get()
+        {
+            return _context.Tasks;
+        }
+
+        [HttpGet("{id}")]
+        public Task Get(int id)
+        {
+            return _context.Tasks.FirstOrDefault(myTask => myTask.Id == id);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Task task)
+        {
+            if(task == null)
+            {
+                return BadRequest();
+            }
+
+            if (_context.Tasks.Any(contextTask => contextTask.Id == task.Id))
+            {
+                return BadRequest(new ConflictResult());
+            }
+
+            Task addedTask = _context.Tasks.Add(task).Entity;
+            _context.SaveChanges();
+            return Accepted(addedTask);
+        }
+
+        [HttpPut]
+        public IActionResult Put(Task task)
+        {
+            if(task == null)
+            {
+                return BadRequest();
+            }
+
+            _context.Tasks.Update(task);
+            _context.SaveChanges();
+            return Accepted();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            Task targetTask = _context.Tasks.FirstOrDefault(task => task.Id == id);
+            if (targetTask == null)
+            {
+                return BadRequest();
+            }
+
+            _context.Tasks.Remove(targetTask);
+            _context.SaveChanges();
+            return Accepted();
+        }
+    }
+}
---
+++ API/Data/Migrations/20220806182201_Initial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Workspace.API.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    Deadline = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    IsCompleted = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tasks");
+        }
+    }
+}
